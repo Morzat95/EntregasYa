bdc02cc530984e7ccd253ff8c0e59d2e5b850833
Federico <fe_de_qui9@hotmail.com>
2019-11-13 00:57:29 -0300

    Agregando mapa a página nuevo pedido y dibujando repartidores disponibles

---
+++ js/myJS/drawer.js
@@ -0,0 +1,31 @@
+var Drawer = function() {
+    return {
+        drawDriverInMap: drawDriverInMap,
+        drawTypesInList: drawTypesInList
+    }
+
+    /******************************************************************************
+     * Función para dibujar un repartidor en un mapa.
+     */
+    function drawDriverInMap(driver, map) {
+        console.log("Dibujando el repartidor: " + driver.id);
+
+		var info = driver.name;
+		// Creamos un marker.		
+		var p = L.marker(L.latLng(driver.positions[0].lat, driver.positions[0].lon))
+			.bindPopup(info);
+
+		p.addTo(map);		
+	}
+
+    /******************************************************************************
+     * Función para listar los tipos de incidente en la página.
+     */
+    function drawTypesInList(types, nodeId) {        
+		types.forEach(function(type) {
+            var li = $('<li>');
+            li.append(type.description + " (" + type.delay +")");
+            $("#"+nodeId).append(li);
+        });
+    }
+}
