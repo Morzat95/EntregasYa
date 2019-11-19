b4f4746d8b41ae23afa1d49cae0149032debd99c
Federico <fe_de_qui9@hotmail.com>
2019-11-19 17:34:37 -0300

    Cleaning repo

--- js/config.js
+++
@@ -1,6 +0,0 @@
-var heroku = "https://entregasya.herokuapp.com/api";
-var local = "http://localhost:3000/api";
-
-var Config = {
-    url: heroku
-}
--- js/drawer.js
+++ js/drawer.js
@@ -1,9 +1,25 @@
 var Drawer = function() {
     return {
+        drawDriverInMap: drawDriverInMap,
         drawIncidentInMap: drawIncidentInMap,
         drawTypesInList: drawTypesInList
     }
 
+    /******************************************************************************
+     * Función para dibujar un repartidor en un mapa.
+     */
+    function drawDriverInMap(driver, map) {
+        console.log("Dibujando el repartidor: " + driver.id);
+
+        scoreIcon = Config.scoreIcon;
+		var info = driver.name + ' ' + scoreIcon.repeat(driver.score);
+		// Creamos un marker.
+		var p = L.marker(L.latLng(driver.positions[0].lat, driver.positions[0].lon))
+			.bindPopup(info);
+
+		p.addTo(map);
+    }
+    
     /******************************************************************************
      * Función para dibujar un incidente en un mapa.
      */
@@ -21,7 +37,7 @@ var Drawer = function() {
     /******************************************************************************
      * Función para listar los tipos de incidente en la página.
      */
-    function drawTypesInList(types, nodeId) {        
+    function drawTypesInList(types, nodeId) {
 		types.forEach(function(type) {
             var li = $('<li>');
             li.append(type.description + " (" + type.delay +")");
